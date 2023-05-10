33251841ff766484d7b0e98585e8888847676c28
Arturo <arturo@grupomundial.cr>
2023-05-10 00:04:25 -0400

    Update data of CV to 2023.

--- scripts/scriptCV.js
+++ scripts/scriptCV.js
@@ -1,47 +1,30 @@
 let dictionarySpanish = {
-    profile: "Trabajo en informática como profesional desde hace 7 años, he colaborado en muchos proyectos de TI en diferentes áreas de negocio (publico/privado) integrando equipos con diversos requerimientos y personas, siempre logrando integrarme de manera positiva y dinámica, aportando esfuerzo e ideas que contribuyan al alcance de las metas y objetivos en los tiempos definidos. Por otro lado, durante estos años he desarrollado y mejorado poco a poco mis habilidades personales y profesionales, buscando dar lo mejor siempre en mi trabajo.",
+    profile: "Trabajo en informática como profesional desde hace 8 años, he colaborado en muchos proyectos de TI en diferentes áreas de negocio (publico/privado) integrando equipos con diversos requerimientos y personas, siempre logrando integrarme de manera positiva y dinámica, aportando esfuerzo e ideas que contribuyan al alcance de las metas y objetivos en los tiempos definidos. Por otro lado, durante estos años he desarrollado y mejorado poco a poco mis habilidades personales y profesionales, buscando dar lo mejor siempre en mi trabajo.",
     printHtml: "Imprimir este CV ...",
     downloadPDF: "Descargar este CV en PDF ...",
     btnLanguage: "Cambiar a inglés",
     workExperience: "Experiencia",
+
+    workExperienceHeader0: "Middle Software Engineer",
+    workExperienceDate0: "SGM | 2023(Enero) - Present",
+    workExperienceDescription0: "Trabajando en análisis, diseño, programación, pruebas, mantenimiento, implementación, mejora, soporte de sistemas, bases de datos.",
+
     workExperienceHeader1: "Profesor",
     workExperienceDate1: "UCR | 2022(mar) - Presente",
     workExperienceDescription1: "Conceptos, análisis y desarrollo de la planificación estratégica del área de TI, alineamiento de TI con el plan estratégico de la organización, visión general de la organización del departamento de TI, selección de proyectos, planes de contingencia, análisis de presupuesto y metodologías de compra de tecnología.",
+
     workExperienceHeader2: "Middle Software Engineer",
     workExperienceDate2: "Crux | 2020(mar) - Presente",
     workExperienceDescription2: "Trabajando en análisis, diseño, programación, pruebas, mantenimiento, implementación, mejora, soporte de sistemas, bases de datos.",
+
     workExperienceHeader3: "Full Stack Developer",
     workExperienceDate3: "INEC | 2017(jun) - 2020(feb)",
     workExperienceDescription3: "Departamento de TI, realizando funciones de análisis, diseño, programación, pruebas, mantenimiento y evolución, implementación, soporte de sistemas, bases de datos.",
+
     workExperienceHeader4: "Full Stack Developer",
     workExperienceDate4: "Grupo Colono | 2015(ene) - 2017(may)",
     workExperienceDescription4: "Departamento de TI (Costa Rica, Panamá, Nicaragua y Honduras), realizando funciones de soporte de sistemas, programación, análisis de sistemas y bases de datos.",
-    projects: "Proyectos",
-
-    projectHeader1: "BCCR-CEA",
-    projectDate1: "Crux | 2020(mar) - Presente",
-    projectDescription1: "Proyecto de estadísticas económicas.",
-
-    projectHeader2: "INEC-IPC",
-    projectDate2: "INEC | 2019(Jun) - 2020(feb)",
-    projectDescription2: "El Índice de Precios al Consumidor (IPC) es un indicador estadístico que mide la variación de los precios de abarrotes y servicios representativos del consumo de los hogares costarricenses, en un período de tiempo determinado. Este indicador es una fuente de información para estimar la inflación, actualizar la política monetaria y, en general, para la planificación económica del país. También se utiliza en ajustes de salarios, reajustes de contratos de alquiler, cálculo de modelos tarifarios, para estimar el cambio en el poder adquisitivo, entre otros.",
-
-    projectHeader3: "INEC-ENUT",
-    projectDate3: "INEC | 2019(ene) - 2019(may)",
-    projectDescription3: "La Encuesta Nacional de Uso del Tiempo (ENUT), brinda los insumos estadísticos necesarios para visibilizar todas las formas de trabajo de las personas, tanto fuera como dentro del hogar, sean remuneradas o no. Los datos presentados y la información que se puede obtener a través del desarrollo de indicadores, permiten monitorear las brechas en el uso del tiempo de mujeres y hombres en las diferentes tareas cotidianas, ya sea en el ámbito laboral, comunitario y familiar.",
-
-    projectHeader4: "INEC-ENADIS",
-    projectDate4: "INEC | 2018(ene) - 2019(dic)",
-    projectDescription4: "La Encuesta Nacional sobre Discapacidad (ENADIS) es la operación estadística en el país, que mide la prevalencia y características de la discapacidad para la formulación, seguimiento y evaluación de políticas públicas, y acciones privadas que promuevan la autonomía y el ejercicio de los derechos de las personas con discapacidad.",
-
-    projectHeader5: "INEC-ENAHO",
-    projectDate5: "INEC | 2017(jun) - 2017(dic)",
-    projectDescription5: "La Encuesta Nacional de Hogares (ENAHO) es un programa de recolección de datos cuyo enfoque está asociado al nivel de bienestar de la población, especialmente enfocado a la composición del ingreso de los hogares, su distribución y características de los hogares y de la población en situación de pobreza. También incluye el estudio de la propiedad de la vivienda y sus características, el acceso de las personas a la educación ya la seguridad social, así como la población que trabaja y las condiciones de estos empleos, entre otros.",
-
-    projectHeader6: "Colono-ERP ",
-    projectDate6: "Grupo Colono | 2015(ene) - 2017(may)",
-    projectDescription6: "Se compone de módulos integrados en un solo sistema que se comunican entre sí y comparten una base de datos común. Algunos módulos como finanzas, contabilidad, recursos humanos, ventas, compras, presupuestos, flujos de caja, almacén, pedidos, inventarios y otros.",
-
+    
     skills: "Habilidades",
     skillsTechnical: "Técnico",
     skillTechnical1: "JS/React/TypeScript/Node.js",
@@ -59,22 +42,6 @@ let dictionarySpanish = {
     skillProfessional2: "Trabajo en equipo",
     skillProfessional3: "Fuerte solución de problemas",
     skillProfessional4: "Buena administración del tiempo",
-
-    education: "Educación",
-    educationDescription1: "Lic. Ingeniería de Sistemas",
-    educationInstitution1: "UACA",
-    educationDate1: "2017 - 2020",
-
-    educationDescription2: "Bach. Informática Empresarial",
-    educationInstitution2: "UCR",
-    educationDate2: "2010 - 2015",
-
-    certifications: "Certificaciones",
-    certificationDescription1: "Scrum Foundation Professional Certificate (SFPC) ",
-    certificationDate1: "Certiprof (2020)",
-
-    certificationDescription2: "DevOps Essentials Professional Certificate (DEPC) ",
-    certificationDate2: "Certiprof (2020)",
 
     languages: "Idiomas",
     language1: "Español (Nativo)",
@@ -94,44 +61,27 @@ let dictionaryEnglish = {
     downloadPDF: "Download this CV in PDF ...",
     btnLanguage: "Change to spanish",
     workExperience: "Work Experiences",
+
+    workExperienceHeader0: "Middle Software Engineer",
+    workExperienceDate0: "SGM | 2023(Enero) - Present",
+    workExperienceDescription0: "Working on analysis, design, programming, testing, maintenance, implementation, improvement, systems support, databases.",
+
     workExperienceHeader1: "Teacher",
     workExperienceDate1: "UCR | 2022(mar) - Present",
     workExperienceDescription1: "Concepts, analysis and development of the strategic planning of the IT area, alignment of IT with the strategic plan of the organization, overview of the organization of the IT department, project selection, contingency plans, budget analysis and methodologies to purchase technology.",
+
     workExperienceHeader2: "Middle Software Engineer",
     workExperienceDate2: "Crux | 2020(mar) - Present",
     workExperienceDescription2: "Working on analysis, design, programming, testing, maintenance, implementation, improvement, systems support, databases.",
+
     workExperienceHeader3: "Full Stack Developer",
     workExperienceDate3: "INEC | 2017(jun) - 2020(feb)",
     workExperienceDescription3: "IT Department, performing functions of analysis, design, programming, testing, maintenance and evolution, implementation, systems support, databases.",
+    
     workExperienceHeader4: "Full Stack Developer",
     workExperienceDate4: "Grupo Colono | 2015(jan) - 2017(may)",
     workExperienceDescription4: "IT Department (Costa Rica, Panama, Nicaragua and Honduras), performing systems support functions, programming, systems analysis and databases.",
-    projects: "Projects",
-
-    projectHeader1: "BCCR-CEA",
-    projectDate1: "Crux | 2020(mar) - Present",
-    projectDescription1: "Economic statistics project.",
-
-    projectHeader2: "INEC-IPC",
-    projectDate2: "INEC | 2019(Jun) - 2020(feb)",
-    projectDescription2: "The Índice de Precios al Consumidor (IPC) is a statistical indicator that measures the variation in the prices of groceries and services representative of the consumption of Costa Rican households, in a given period of time. This indicator is a source of information to estimate inflation, update monetary policy and, in general, for the economic planning of the country. It is also used in salary adjustments, readjustment of rental contracts, calculation of rate models, to estimate the change in purchasing power, among others.",
-
-    projectHeader3: "INEC-ENUT",
-    projectDate3: "INEC | 2019(jan) - 2019(may)",
-    projectDescription3: "The Encuesta Nacional de Uso del Tiempo (ENUT), provides statistical inputs necessary to make visible all forms of work of people, both outside and inside the home, whether they are paid or not. The data presented and the information that can be obtained through the development of indicators make it possible to monitor the gaps in the use of time by women and men in the different daily tasks, whether in the areas of work, community and family life.",
-
-    projectHeader4: "INEC-ENADIS",
-    projectDate4: "INEC | 2018(jan) - 2019(dec)",
-    projectDescription4: "The Encuesta Nacional sobre Discapacidad (ENADIS) is the statistical operation in the country, which measures the prevalence and characteristics of disability for the formulation, monitoring and evaluation of public policies, and private actions that promote autonomy and the exercise of the rights of people with disabilities.",
-
-    projectHeader5: "INEC-ENAHO",
-    projectDate5: "INEC | 2017(jun) - 2017(dec)",
-    projectDescription5: "The Encuesta Nacional de Hogares (ENAHO) is a data collection program whose focus is associated with the level of well-being of the population, especially focused on the composition of household income, its distribution and characteristics of households and the population living in poverty. It also includes the study of housing ownership and its characteristics, people's access to education and social security, as well as the population that works and the conditions of these jobs, among others.",
-
-    projectHeader6: "Colono-ERP ",
-    projectDate6: "Grupo Colono | 2015(jan) - 2017(may)",
-    projectDescription6: "It is made up of integrated modules or business applications that talk to each other and share a common database. Some modules finance, accounting, human resources, sales, purchases, budgets, cash flows, warehouse, orders, inventories and others.",
-
+    
     skills: "Skills",
     skillsTechnical: "Technical",
     skillTechnical1: "JS/React/TypeScript/Node.js",
@@ -148,23 +98,7 @@ let dictionaryEnglish = {
     skillProfessional1: "Effective communication",
     skillProfessional2: "Team player",
     skillProfessional3: "Strong problem solver",
-    skillProfessional4: "Good time management",
-
-    education: "Education",
-    educationDescription1: "Lic. Ingeniería de Sistemas",
-    educationInstitution1: "UACA",
-    educationDate1: "2017 - 2020",
-
-    educationDescription2: "Bach. Informática Empresarial",
-    educationInstitution2: "UCR",
-    educationDate2: "2010 - 2015",
-
-    certifications: "Certifications",
-    certificationDescription1: "Scrum Foundation Professional Certificate (SFPC) ",
-    certificationDate1: "Certiprof (2020)",
-
-    certificationDescription2: "DevOps Essentials Professional Certificate (DEPC) ",
-    certificationDate2: "Certiprof (2020)",
+    skillProfessional4: "Good time management",  
 
     languages: "Languages",
     language1: "Spanish (Native)",
